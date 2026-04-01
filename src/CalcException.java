a85ae09c88b6694c302c8fce763d8ba12641f2e6
Anand Pandey <ap9012928@gmail.com>
2026-04-01 16:05:22 +0530

    Exception handling files added

---
+++ src/CalcException.java
@@ -0,0 +1,18 @@
+// Single exception type for all interpreter errors (tokenizer, parser, runtime)
+// Format: [TOKENIZER] line 3: Unexpected character '@'
+public class CalcException extends RuntimeException {
+
+    public enum Phase { TOKENIZER, PARSER, RUNTIME }
+
+    private final Phase phase;
+    private final int   line;
+
+    public CalcException(Phase phase, int line, String message) {
+        super(String.format("[%s] line %d: %s", phase.name(), line, message));
+        this.phase = phase;
+        this.line  = line;
+    }
+
+    public Phase getPhase() { return phase; }
+    public int   getLine()  { return line;  }
+}
