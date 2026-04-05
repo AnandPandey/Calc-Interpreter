bfb96a4c33350c85b714a6bf78652a6ddc3d98f7
Anand Pandey <ap9012928@gmail.com>
2026-04-05 11:54:50 +0530

    Add Colors.java for better interaction

---
+++ src/Colors.java
@@ -0,0 +1,7 @@
+public class Colors {
+    public static final String RESET  = "\u001B[0m";
+    public static final String GREEN  = "\u001B[32m";  // output values
+    public static final String CYAN   = "\u001B[36m";  // keywords
+    public static final String RED    = "\u001B[31m";  // errors
+    public static final String YELLOW = "\u001B[33m";  // prompt
+}
